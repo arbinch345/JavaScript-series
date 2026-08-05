8c0c4b660db113096a6bd2250b4fb79eacf09c91
arbinch345 <arbinch345@gmail.com>
2026-08-05 22:18:14 +0545

    First Js Code

---
+++ 01_basics/01_basic.js
@@ -0,0 +1,10 @@
+// JavaScript: popular language which is used in web development, app development
+
+// First Code:
+console.log("Hello world!")
+
+// Printing plain text
+console.log("Learning Js is fun!")
+
+// Printing numbers
+console.log(123)
