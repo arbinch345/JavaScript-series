46e6dc0c16032ef40753035af7a8f20d6992747e
arbinch345 <arbinch345@gmail.com>
2026-08-08 21:50:35 +0545

    Practice question from basics

---
+++ Practice Questions/01_basics.js
@@ -0,0 +1,26 @@
+// Qn.1. Create a const object called "product" to store information shown in the picture.
+const product = {
+    name: "Parker Jotter Standard CT Call Pen",
+    color: "Black",
+    Price: 270,
+    Discount: "5%",
+    Rating: 4,
+    isDeal: true
+}
+
+console.log(product)
+console.log(typeof(product))
+
+
+// Qn.2. Create a const object called "profile" to store information shown in the picture.
+const profile = {
+    User_Name: "Shradha Khapra",
+    posts: 195,
+    follower: "569k",
+    following: 4,
+    Occupation: "Enterpreneur",
+    isFollow: false
+}
+
+console.log(profile)
+console.log(typeof(profile))
