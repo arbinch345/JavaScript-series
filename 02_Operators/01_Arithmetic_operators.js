ebd404ec36ecad0c41c22f4def6924625390fcf4
arbinch345 <arbinch345@gmail.com>
2026-08-09 21:51:25 +0545

    arithmetic operators

---
+++ 02_Operators/01_Arithmetic_operators.js
@@ -0,0 +1,59 @@
+// Operators: Used to perform some operation on data
+
+// Arithmetic Operators: A typical arithmetic operation operates on two numbers.
+
+// add: The addition operator (+) adds numbers.
+let a = 5;
+let b = 7;
+
+console.log(a + b)
+console.log("a + b = ", a+b)
+
+// subtract: The subtraction operator (-) subtracts numbers.
+let m = 255;
+let n = 155;
+
+console.log(m - n)
+console.log("m - n = ", m-n)
+
+// Multiply: The multiplication operator (*) multiplies numbers.
+let i = 5;
+let j = 2;
+
+console.log(i * j)
+console.log("i x j = ", i*j)
+
+// Division: The division operator (/) divides numbers.
+let x = 85;
+let y = 5;
+
+console.log(x / y)
+console.log("x / y = ", x/y)
+
+// Modulus: The modulus operator (%) returns the division remainder.
+let k = 5;
+let l = 2;
+
+console.log(k % l)
+console.log("k % l =",k % l)
+
+// Exponentiation: The exponentiation operator (**) raises the first operand to the power of the second operand.
+let r = 5;
+let s = 2;
+
+console.log(r ** s)
+console.log("r ^ s = ",r ** s)
+
+// Increment (Unary Operator): The increment operator (++) increments numbers.
+let p = 5;
+p++;    // (post) increment: 
+++p;    // (pre) increment
+
+console.log(p)
+console.log("p = ",p)
+
+// Decrement (Unary Operator): The decrement operator (--) decrements numbers.
+let q = 15;
+q--;
+
+console.log(q)
