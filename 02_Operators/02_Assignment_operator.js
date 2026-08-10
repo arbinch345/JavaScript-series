bff6da42037ed3b55cf7488929adf108684c1e42
arbinch345 <arbinch345@gmail.com>
2026-08-10 21:51:45 +0545

    Assignment Operator

---
+++ 02_Operators/02_Assignment_operator.js
@@ -0,0 +1,46 @@
+// Assignment Operator: Assignment operators assign values to variables.
+
+// The "=" Operator: The Simple Assignment Operator assigns a simple value to a variable.
+let x = 10;
+
+console.log(x);
+
+// The "+=" Operator: The Addition Assignment Operator adds a value to a variable.
+let y = 55;
+y += 5;
+
+console.log(y);
+console.log(y += 4);            // add in the new value
+
+// The "-=" Operator: The Subtraction Assignment Operator subtracts a value from a variable.
+let z = 66;
+z -= 6;
+
+console.log("z = ",z);
+
+// The "*=" Operator: The Multiplication Assignment Operator multiplies a variable.
+let a = 5;
+let b = 2;
+
+a *= b;
+console.log("a = ",a)
+
+// The "**=" operator: The Exponentiation Assignment Operator raises a variable to the power of the operand.
+let p = 5;
+let q = 6;
+
+p **= q;
+console.log("p = ", p)
+
+// The "/=" Operator: The Division Assignment Operator divides a variable.
+let r = 10;
+let s = 5;
+
+r /= s;
+console.log("r = ",r)
+
+// The "%=" Operator: The Remainder Assignment Operator assigns a remainder to a variable.
+let n = 10;
+n %= 5;
+
+console.log("n = ",n)
