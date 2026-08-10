7c4d09b754d83bda536abf7525658c6c7919eac3
arbinch345 <arbinch345@gmail.com>
2026-08-10 21:20:54 +0545

    unary operator added

--- 02_Operators/01_Arithmetic_operators.js
+++ 02_Operators/01_Arithmetic_operators.js
@@ -6,54 +6,57 @@
 let a = 5;
 let b = 7;
 
-console.log(a + b)
-console.log("a + b = ", a+b)
+console.log(a + b);
+console.log("a + b = ", a+b);
 
 // subtract: The subtraction operator (-) subtracts numbers.
 let m = 255;
 let n = 155;
 
-console.log(m - n)
-console.log("m - n = ", m-n)
+console.log(m - n);
+console.log("m - n = ", m-n);
 
 // Multiply: The multiplication operator (*) multiplies numbers.
 let i = 5;
 let j = 2;
 
-console.log(i * j)
-console.log("i x j = ", i*j)
+console.log(i * j);
+console.log("i x j = ", i*j);
 
 // Division: The division operator (/) divides numbers.
 let x = 85;
 let y = 5;
 
-console.log(x / y)
-console.log("x / y = ", x/y)
+console.log(x / y);
+console.log("x / y = ", x/y);
 
 // Modulus: The modulus operator (%) returns the division remainder.
 let k = 5;
 let l = 2;
 
-console.log(k % l)
-console.log("k % l =",k % l)
+console.log(k % l);
+console.log("k % l =",k % l);
 
 // Exponentiation: The exponentiation operator (**) raises the first operand to the power of the second operand.
 let r = 5;
 let s = 2;
 
-console.log(r ** s)
-console.log("r ^ s = ",r ** s)
+console.log(r ** s);
+console.log("r ^ s = ",r ** s);
 
 // Increment (Unary Operator): The increment operator (++) increments numbers.
 let p = 5;
-p++;    // (post) increment: 
-++p;    // (pre) increment
+// p++;    
+// ++p;    
 
-console.log(p)
-console.log("p = ",p)
+// console.log(p)
+console.log("p++ = ",p++);      // (post) increment: first print the same value
+console.log("p++ = ", p);        // change the above code value here
+console.log("++p = ", p);     // (pre) increment: First change the value and print
 
 // Decrement (Unary Operator): The decrement operator (--) decrements numbers.
 let q = 15;
-q--;
 
-console.log(q)
+console.log("q-- = ", q--);     // use the value first, then decrement
+console.log("q-- = ",q)
+console.log("--q = ", q);     // decrement first, then use the value
