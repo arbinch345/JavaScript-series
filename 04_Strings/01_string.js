689e6adf1a7979096969f0f7d2cd9f642e97dc87
arbinch345 <arbinch345@gmail.com>
2026-08-18 08:06:24 +0545

    strings

---
+++ 04_Strings/01_string.js
@@ -0,0 +1,10 @@
+// String: sequence of characters used to represent text
+
+// Create string
+let str = "Bro";                    // can define in double quote
+let str2 = 'Hello Bro';             // can define in single quote
+
+console.log(str)
+console.log(str2)
+console.log(str2.length)          // print the length of string
+console.log(str[0])               // index accessing
